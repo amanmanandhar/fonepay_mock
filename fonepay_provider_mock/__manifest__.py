7472a9a8820c73670f576c4e215045b1cea65fff
Aman <amansenpai01@gmail.com>
2026-06-13 21:36:27 +0545

    added the fields for fonepay credential form

--- fonepay_provider_mock/__manifest__.py
+++ fonepay_provider_mock/__manifest__.py
@@ -26,6 +26,7 @@ Long description of module's purpose
         'views/views.xml',
         'views/templates.xml',
         'data/payment_provider_data.xml',
+        'views/payment_provider_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- fonepay_provider_mock/models/payment_provider.py
+++ fonepay_provider_mock/models/payment_provider.py
@@ -8,6 +8,12 @@ class PaymentProvider(models.Model):
         ondelete={'fonepay': 'set default'},
     )
 
+    fonepay_merchant_code = fields.Char(string='Merchant Code',groups='base.group_system')
+    fonepay_username = fields.Char(string='Username',groups='base.group_system')
+    fonepay_password = fields.Char(string='Password',groups='base.group_system')
+    fonepay_secret_key = fields.Char(string='Secret Key',groups='base.group_system')
+    fonepay_test_mode = fields.Boolean(string='Sandbox Mode', groups='base.group_system')
+
     def _is_available(self, *args, **kwargs):
         if self.filtered(lambda p: p.code == 'fonepay'):
             return True
